ad065fca534f8d4d631c4b5e0ade545e68c18189
isidora <is.gomezj@duocuc.cl>
2026-05-19 20:47:13 -0400

    Creación de metodos personalizados en los repositorios de Cart y CartItem

--- carrito/src/main/java/cl/duoc/carrito/repository/CartItemRepository.java
+++ carrito/src/main/java/cl/duoc/carrito/repository/CartItemRepository.java
@@ -3,6 +3,14 @@ package cl.duoc.carrito.repository;
 import cl.duoc.carrito.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
+
+    boolean existsByCartIdAndProductId(Long cartId, Long productId);
+
+    void deleteByCartIdAndProductId(Long cartId, Long productId);
 
 }
